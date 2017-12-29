6579710d1a0528125a2357067d3acc21456a87a8
Nisheed Jagadish 2 <njaga2@sapient.com>
2017-12-29 12:07:38 +0530

    Added dummy files for component creation

---
+++ generators/app/templates/component.js
@@ -0,0 +1,1 @@
+// <component-name> component JS goes here
---
+++ generators/app/templates/component.spec.js
@@ -0,0 +1,13 @@
+// <component-name> spec file
+var expect = chai.expect;
+
+describe('<component-name> Component', function () {
+
+  beforeEach(function () {});
+
+  afterEach(function () {});
+
+  it('should pass dummy test', function (done) {
+    expect(true).to.equal(true);
+  });
+})
