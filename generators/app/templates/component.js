be37b9d5bfb77100da0a69d6fe6a90e5461bfe56
Nisheed Jagadish 2 <njaga2@sapient.com>
2018-01-02 10:53:48 +0530

    Added IFFE to the JS template

--- generators/app/templates/component.js
+++ generators/app/templates/component.js
@@ -1,1 +1,21 @@
 // <%= compNamePretty %> component JS goes here
+(function() {
+  "use strict";
+
+  window['<%= componentsGroup %>'] = window['<%= componentsGroup %>'] || {};
+  /**
+   * <%= compNamePretty %> Class.
+   */
+  function <%= compName %>() {
+    /**
+     * Function called when the component is available on screen
+     */
+    function initialize () {
+      // Component functionality goes here.
+
+    }
+  };
+
+  window['<%= componentsGroup %>']['<%= compName %>'] = new <%= compName %>();
+  window['<%= componentsGroup %>']['<%= compName %>'].initialize();
+})();
